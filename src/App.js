475a86429b6df16195701ff632a318b6fd180e31
lakshmi331 <chinnagangollalakshmidevi331@gmail.com>
2023-02-21 08:27:13 +0000

    remove boolean

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import {Heading, CustomButton} from './styledComponents'
 const App = () => (
   <>
     <Heading>Hello World</Heading>
-    <CustomButton type="button" outline={false}>
-      Click
-    </CustomButton>
-    <CustomButton type="button" outline={true}>
+    <CustomButton type="button">Click</CustomButton>
+    <CustomButton type="button" outline>
       Click
     </CustomButton>
   </>
