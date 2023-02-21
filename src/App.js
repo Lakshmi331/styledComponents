93336907287a4e50520bbacd3bc8949630a36c4a
lakshmi331 <chinnagangollalakshmidevi331@gmail.com>
2023-02-21 08:09:08 +0000

    added outline button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import {Heading, CustomButton} from './styledComponents'
 const App = () => (
   <>
     <Heading>Hello World</Heading>
-    <CustomButton>Click</CustomButton>
+    <CustomButton type="button" color="#ffffff" bgColor="#0070c1">
+      Click
+    </CustomButton>
+    <CustomButton type="button" color="#0070c1" bgColor="#ffffff">
+      Click
+    </CustomButton>
   </>
 )
 
--- src/styledComponents.js
+++ src/styledComponents.js
@@ -9,8 +9,8 @@ export const CustomButton = styled.button`
   padding: 10px;
   margin-right: 20px;
   font-size: 15px;
-  color: #ffffff;
+  color: ${props => props.color};
   border-radius: 4px;
   border: 2px solid #0070c1;
-  background-color: #0070c1;
+  background-color: ${props => props.bgColor};
 `
